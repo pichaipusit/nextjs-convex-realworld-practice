d8dc9e6c1c387be8f760407817ddb57b2e9cf980
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-05-05 00:34:49 +0700

    feat: hold a message to show actions

--- src/app/react-focus/practice/_chat/ChatBubble.tsx
+++ src/app/react-focus/practice/_chat/ChatBubble.tsx
@@ -1,6 +1,6 @@
 import { Message } from "@/lib/fake-db";
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ChatActions from "./ChatActions";
 
 type ChatBubbleProps = {
@@ -9,11 +9,25 @@ type ChatBubbleProps = {
 const ChatBubble = ({ message }: ChatBubbleProps) => {
   const { userId, text } = message;
   const [isActionsOpen, setIsActionsOpen] = useState(false);
+  const holdTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const handlePointerDown = () => {
+    holdTimeout.current = setTimeout(() => {
+      setIsActionsOpen(true);
+    }, 800);
+  };
+  const handlePointerUp = () => {
+    if (holdTimeout.current) {
+      clearTimeout(holdTimeout.current);
+      holdTimeout.current = null;
+    }
+  };
 
   return (
     <div>
       <li
-        onClick={() => setIsActionsOpen(true)}
+        onPointerDown={handlePointerDown}
+        onPointerUp={handlePointerUp}
         className={cn(
           "p-2 w-fit",
           userId === "user-1" ? "bg-gray-200" : "bg-sky-200 ml-auto"
