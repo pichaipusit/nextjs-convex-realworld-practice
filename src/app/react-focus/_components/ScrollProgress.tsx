ccbe4765a45b8ed48100c5e75b985f2a3a383ccf
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-05-05 19:36:53 +0700

    feat: create scroll progress component

---
+++ src/app/react-focus/_components/ScrollProgress.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const ScrollProgress = () => {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = document.documentElement.clientHeight;
+
+      const progress = (scrollTop / (scrollHeight - clientHeight)) * 100;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 bg-gray-300">
+      <div
+        className="h-full bg-blue-500"
+        style={{ width: `${scrollProgress}%` }}
+      ></div>
+    </div>
+  );
+};
+export default ScrollProgress;
